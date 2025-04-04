59a039fac8f8b8cdf1d9eaabe1114230045ac734
lakshv06 <lakshyaverma0002@gmail.com>
2025-04-05 00:33:38 +0530

    add otp next using formprovider

---
+++ src/pages/BouncyLogo.tsx
@@ -0,0 +1,9 @@
+import { ReactElement } from "react";
+
+export default function BouncyLogo(): ReactElement{
+    return(
+        <div>
+            <img src="../../public/giphy.webp" alt="" className="bouncy-logo-image"/>
+        </div>
+    )
+}

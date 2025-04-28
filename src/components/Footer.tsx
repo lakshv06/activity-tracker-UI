06ac23aaf385a62c5efcab793f80e89872013d98
lakshv06 <lakshyaverma0002@gmail.com>
2025-04-29 02:19:32 +0530

    changes in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,29 @@
-function Footer (){
-    return(
-        <div id = "body-footer">
-            Made In India with 💚 by Laksh
-        </div>
-    )
+function Footer() {
+  return (
+    <div id="body-footer">
+      Made In India with 💚 by Laksh{" "}
+      <span>
+        {" "}
+        <a
+          href="https://github.com/lakshv06"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-github" style={{ fontSize: "1.5rem" }}></i>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/lakshya-verma-492a51174/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i
+            className="fab fa-linkedin"
+            style={{ fontSize: "1.5rem", marginLeft: "0.625rem" }}
+          ></i>
+        </a>
+      </span>
+    </div>
+  );
 }
 
 export default Footer;
